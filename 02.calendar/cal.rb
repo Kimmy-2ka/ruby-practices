20e9f4972d866e13da04a3cf69f96acd593c3d80
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-04-29 20:05:39 -0700

    レビュー結果を元に修正した

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,30 +9,20 @@ opt.parse(ARGV)
 
 require "date"
 today = Date.today
-if params[:month]
-  target_month = params[:month].to_i
-else
-  target_month = today.month
-end
-if params[:year]
-  target_year = params[:year].to_i
-else
-  target_year = today.year
-end
+target_month = params[:month] ? params[:month].to_i : today.month
+target_year = params[:year] ? params[:year].to_i : today.year
+
 first_day = Date.new(target_year, target_month, 1)
 last_day = Date.new(target_year, target_month, -1)
-print (first_day.strftime("%B") + " " + first_day.year.to_s).center(21)
+
+print [first_day.strftime("%B"), first_day.year.to_s].join(' ').center(21)
 puts
 ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"].each do |weekday|
   print weekday.rjust(3)
 end
 puts
+print "   " * first_day.wday
 (first_day..last_day).each do |x|
-  if first_day == x
-    print "   " * first_day.wday
-  end
   print x.day.to_s.rjust(3)
-  if x.strftime("%a") == "Sat"
-    puts
-  end
+  puts if x.saturday?
 end
