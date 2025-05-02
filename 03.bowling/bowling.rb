db1a00c54de5cd5054bc6808bb2bcf71eae60378
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-05-02 14:03:03 -0700

    ボウリングのスコア計算プログラムを作成

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.split(',')
+
+shots = []
+scores.each do |shot|
+  shot == 'X' ? shots.push(10, 0) : shots << shot.to_i
+end
+
+frames = shots[0..19].each_slice(2).to_a
+frames << shots[20..] if shots[20] # 10フレーム目に3投目以降がある場合
+
+point = 0
+frames.each_with_index do |frame, idx|
+  if idx > 8
+    point += frame.sum
+    next
+  end
+
+  following_frame = frames[idx + 1]
+  first_bonus = following_frame[0] || 0
+  second_bonus = first_bonus == 10 ? frames[idx + 2][0] : following_frame[1]
+
+  point +=
+    if frame[0] == 10
+      frame.sum + first_bonus + second_bonus
+    elsif frame.sum == 10
+      frame.sum + first_bonus
+    else
+      frame.sum
+    end
+end
+
+puts point
