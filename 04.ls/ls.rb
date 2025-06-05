35cdcb41000324f17844e2d7adfb4deb855969d6
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-06-05 14:05:20 -0700

    lsコマンド1のレビュー結果を反映2：column_count を定数に変更、繰り返し方法をtimesに変更、ファイルの出力式を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+COLUMN_COUNT = 3
+
 def main
   files = Dir.glob('*').sort_by(&:downcase)
-  column_count = 3
-  row_count = files.count.ceildiv(column_count)
-  columns = split_into_columns(files, column_count, row_count)
+  row_count = files.count.ceildiv(COLUMN_COUNT)
+  columns = split_into_columns(files, COLUMN_COUNT, row_count)
   output(columns, row_count)
 end
 
@@ -19,11 +20,11 @@ def split_into_columns(files, column_count, row_count)
 end
 
 def output(columns, row_count)
-  column_widths = columns.map { |words| words.map(&:size).max || 0 }
-  (0...row_count).each do |row_idx|
+  column_widths = columns.map { |words| words.map(&:size).max }
+  row_count.times do |row_idx|
     columns.each_with_index do |col, col_idx|
       filename = col[row_idx]
-      print "#{filename.to_s.ljust(column_widths[col_idx])}  "
+      print "#{filename.ljust(column_widths[col_idx])}  " if filename
     end
     print "\n"
   end
