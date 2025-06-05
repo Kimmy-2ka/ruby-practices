fb1961cfd86258c20f5eb8ed2d0731dcce8b610b
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-06-04 22:21:48 -0700

    lsコマンド1のレビュー結果を反映：ceildivの使用、変数名の変更、filenameの式を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,7 +4,7 @@
 def main
   files = Dir.glob('*').sort_by(&:downcase)
   column_count = 3
-  row_count = (files.count.to_f / column_count).ceil
+  row_count = files.count.ceildiv(column_count)
   columns = split_into_columns(files, column_count, row_count)
   output(columns, row_count)
 end
@@ -12,8 +12,8 @@ end
 def split_into_columns(files, column_count, row_count)
   column_group = Array.new(column_count) { [] }
   files.each_with_index do |file, index|
-    col_order = index / row_count
-    column_group[col_order] << file
+    col_index = index / row_count
+    column_group[col_index] << file
   end
   column_group
 end
@@ -23,7 +23,7 @@ def output(columns, row_count)
   (0...row_count).each do |row_idx|
     columns.each_with_index do |col, col_idx|
       filename = col[row_idx]
-      print "#{(filename || ' ').ljust(column_widths[col_idx], ' ')}  "
+      print "#{filename.to_s.ljust(column_widths[col_idx])}  "
     end
     print "\n"
   end
