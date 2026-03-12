d3e8df971a351b14f4431c57e5fd8334465befdf
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-12 22:12:32 +0900

    -rオプションを実装

--- 07.ls_object/lib/command.rb
+++ 07.ls_object/lib/command.rb
@@ -6,24 +6,24 @@ require_relative 'formatter'
 class Command
   # オプションLがついているかを確認する
   def initialize(params, pathname)
-    files =
+    @entries = build_entries(params, pathname)
+  end
+
+  # Entryの情報を持ったentriesを作成する。
+  def build_entries(params, pathname)
+    entry_names = prepare_entry_names(params, pathname)
+    entry_names.map { |name| Entry.new(name) }
+  end
+
+  def prepare_entry_names(params, pathname)
+    entry_names =
       if params[:all]
         Dir.entries(pathname)
       else
         Dir.glob('*', base: pathname)
       end.sort
 
-    @entries = build_entries(files)
-  end
-
-  # Entryの情報を持ったentriesを作成する。
-  def build_entries(files)
-    entries = []
-    files.each do |file|
-      entry = Entry.new(file)
-      entries << entry
-    end
-    entries
+    params[:reverse] ? entry_names.reverse : entry_names
   end
 
   # 表示する
--- 07.ls_object/test/ls_test.rb
+++ 07.ls_object/test/ls_test.rb
@@ -13,13 +13,23 @@ class LsTest < Minitest::Test
     assert_equal expected, list.output
   end
 
-  def test_output_with_a_option
+  def test_output_with_all_option
     list = Command.new({all: true}, 'test/fixtures/sample_entries')
     expected = <<~TEXT.chomp
       .           Egg.txt      cider
       ..          Frenchfries  diet_sparkling.txt
       .melon.txt  apple.txt
       Apple2.txt  banana
+    TEXT
+    assert_equal expected, list.output
+  end
+
+  def test_output_with_reverse_option
+    list = Command.new({reverse: true}, 'test/fixtures/sample_entries')
+    expected = <<~TEXT.chomp
+      diet_sparkling.txt  apple.txt    Apple2.txt
+      cider               Frenchfries
+      banana              Egg.txt
     TEXT
     assert_equal expected, list.output
   end
