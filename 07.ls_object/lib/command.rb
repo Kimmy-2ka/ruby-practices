26829aa05fe974e5f15c2d18ba7891c3d5db31bd
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-14 14:07:08 +0900

    Commandクラスの内部処理を整理 - private化 - メソッドのリファクタ

--- 07.ls_object/lib/command.rb
+++ 07.ls_object/lib/command.rb
@@ -8,7 +8,13 @@ class Command
     @entries = build_entries(params, pathname)
   end
 
-  # Entryの情報を持ったentriesを作成する。
+  def output(params)
+    formatter = Formatter.new(@entries)
+    params[:long] ? formatter.long_format : formatter.short_format
+  end
+
+  private
+
   def build_entries(params, pathname)
     entry_names = prepare_entry_names(params, pathname)
     entry_names.map do |name|
@@ -19,21 +25,8 @@ class Command
 
   def prepare_entry_names(params, pathname)
     entry_names =
-      if params[:all]
-        Dir.entries(pathname)
-      else
-        Dir.glob('*', base: pathname)
-      end.sort
+      params[:all] ? Dir.entries(pathname).sort : Dir.glob('*', base: pathname, sort: true)
 
     params[:reverse] ? entry_names.reverse : entry_names
-  end
-
-  # 表示する
-  def output(params)
-    if params[:long]
-      Formatter.new(@entries).long_format
-    else
-      Formatter.new(@entries).short_format
-    end
   end
 end
