82baa86b991748076eb98702f3756975099cb6ab
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-03-14 23:13:56 +0900

    Formatterクラスをリファクタリング

--- 07.ls_object/lib/formatter.rb
+++ 07.ls_object/lib/formatter.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'entry'
-
 class Formatter
   COLUMN_COUNT = 3
 
@@ -26,10 +24,11 @@ class Formatter
     '7' => 'rwx'
   }.freeze
 
+  # idxは特殊権限に置き換える対象がowner/group/othersのどれかを表している
   SPECIAL_FLAGS = {
-    '1' => { idx: 2, with_x: 't', without_x: 'T' },
-    '2' => { idx: 1, with_x: 's', without_x: 'S' },
-    '4' => { idx: 0, with_x: 's', without_x: 'S' }
+    '1' => { idx: 2, with_x: 't', without_x: 'T' },  # others
+    '2' => { idx: 1, with_x: 's', without_x: 'S' },  # group
+    '4' => { idx: 0, with_x: 's', without_x: 'S' }   # owner
   }.freeze
 
   def initialize(entries)
@@ -37,79 +36,78 @@ class Formatter
   end
 
   def short_format
-    entries = @entries.map(&:name)
-    # 行の長さを計算する
-    row_count = entries.count.ceildiv(COLUMN_COUNT)
-    # 列ごとの配列を作って長さを計算
-    columns = build_columns(entries, row_count)
-    format_table(row_count, columns)
-  end
-
-  def build_columns(entries, row_count)
-    raw_columns = entries.each_slice(row_count).to_a
-    pad_columns(raw_columns)
-  end
-
-  def pad_columns(raw_columns)
-    column_widths = raw_columns.map { |column| column.map(&:size).max }
-    raw_columns.each_with_index.map do |column, index|
-      column.map { |entry| entry&.ljust(column_widths[index]) }
-    end
-  end
-
-  def format_table(row_count, columns)
-    row_count.times.map do |row_idx|
-      columns.filter_map { |column| column[row_idx] }.join('  ').rstrip
-    end.join("\n")
+    names = @entries.map(&:name)
+    row_count = names.count.ceildiv(COLUMN_COUNT)
+    columns = names.each_slice(row_count).to_a
+    padded_columns = pad_columns(columns)
+    format_table(row_count, padded_columns)
   end
 
   def long_format
     total = "合計 #{@entries.sum(&:blocks) / 2}"
-    max_widths = build_max_widths
-
-    body =
-      @entries.map do |entry|
-        [format_mode(entry),
-         entry.nlink.to_s.rjust(max_widths[:nlink]),
-         entry.user.ljust(max_widths[:user]),
-         entry.group.ljust(max_widths[:group]),
-         entry.size.to_s.rjust(max_widths[:size]),
-         entry.mtime,
-         entry.name].join(' ')
-      end.join("\n")
+    body = format_body
 
     [total, body].join("\n")
   end
 
+  private
+
+  def pad_columns(raw_columns)
+    column_widths = raw_columns.map { |column| column.map(&:size).max }
+    raw_columns.each_with_index.map do |column, idx|
+      column.map { |entry| entry.ljust(column_widths[idx]) }
+    end
+  end
+
+  def format_table(row_count, columns)
+    row_count.times.map do |idx|
+      columns.filter_map { |column| column[idx] }.join('  ').rstrip
+    end.join("\n")
+  end
+
+  def format_body
+    max_widths = build_max_widths
+    @entries.map do |entry|
+      [
+        format_type_and_mode(entry),
+        entry.nlink.to_s.rjust(max_widths[:nlink]),
+        entry.user.ljust(max_widths[:user]),
+        entry.group.ljust(max_widths[:group]),
+        entry.size.to_s.rjust(max_widths[:size]),
+        entry.mtime,
+        entry.name
+      ].join(' ')
+    end.join("\n")
+  end
+
   def build_max_widths
-    {
-      nlink: @entries.map { |entry| entry.nlink.to_s.size }.max,
-      user: @entries.map { |entry| entry.user.to_s.size }.max,
-      group: @entries.map { |entry| entry.group.to_s.size }.max,
-      size: @entries.map { |entry| entry.size.to_s.size }.max
-    }
+    %i[nlink user group size].to_h do |key|
+      width = @entries.map { |entry| entry.public_send(key).to_s.size }.max
+      [key, width]
+    end
+  end
+
+  def format_type_and_mode(entry)
+    type = FILE_TYPES[entry.type]
+    mode = format_mode(entry)
+
+    [type, mode].join
   end
 
   def format_mode(entry)
-    type = FILE_TYPES[entry.type]
-    permission_numbers = entry.mode.slice(-3..-1).chars
-    special_flag = entry.mode.slice(-4, 1)
+    digits = entry.mode.slice(-3..-1).chars
+    special_mode_bit = entry.mode.slice(-4, 1)
+    special_flag = SPECIAL_FLAGS[special_mode_bit]
 
-    permissions = build_permissions(permission_numbers, special_flag)
-    [type, permissions].join
+    digits.map.with_index do |digit, idx|
+      permission = PERMISSIONS[digit]
+
+      special_flag && idx == special_flag[:idx] ? apply_special_permission(permission, special_flag) : permission
+    end.join
   end
 
-  def build_permissions(permission_numbers, special_flag)
-    permission_numbers.map.with_index do |number, idx|
-      permission = PERMISSIONS[number]
-      flag = SPECIAL_FLAGS[special_flag]
-
-      if flag && idx == flag[:idx]
-        new_execute = permission[2] == 'x' ? flag[:with_x] : flag[:without_x]
-        permission.sub(/.$/, new_execute)
-      else
-        permission
-      end
-    end.join
+  def apply_special_permission(permission, special_flag)
+    execute_char = permission[2] == 'x' ? special_flag[:with_x] : special_flag[:without_x]
+    permission.sub(/.$/, execute_char)
   end
 end
